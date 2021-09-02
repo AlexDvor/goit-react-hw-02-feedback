cb08ecbe4959d654c05017d0632fed91ded9209c
Oleksii Matviichuk <alexmatvichuk@gmail.com>
2021-09-02 20:25:15 +0200

    added FeedBackStats

--- src/components/FeedBackStat/FeedBackStat.js
+++ src/components/FeedBackStat/FeedBackStat.js
@@ -2,13 +2,17 @@
 // import style from './FeedBackStat.module.css'
 
 function FeedBackStat({ state }) {
+    const { good, neutral, bad } = state;
   
     return (
         <>
             <h3>statistics</h3>
-            <p>good:{state.good}</p>
-            <p>neutral:{state.neutral}</p>
-            <p>bad:{state.bad}</p>
+            <p>good: {good}</p>
+            <p>neutral: {neutral}</p>
+            <p>bad: {bad}</p>
+            <p>total: {good+neutral+bad}</p>
+            <p>positive feedback:</p>
+        
         </>
     )
 }
