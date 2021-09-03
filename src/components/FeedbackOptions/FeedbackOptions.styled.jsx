5db09ab881c52fd8da21d64ed439627e247ba39c
Oleksii Matviichuk <alexmatvichuk@gmail.com>
2021-09-03 12:32:44 +0200

    updated style btn

--- src/components/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -4,10 +4,11 @@ import styled from '@emotion/styled'
 export const Buttons = styled.button`
 padding:5px 10px;
 margin-right: 20px;
+border-radius:5px;
+border:none;
 
 
 &:hover,
 &:focus {
   background-color: tomato;
-outline:none;
 `
